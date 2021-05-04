03ddc4ddd3bbb25e8a9eaa17bf2bd1e95db609ee
Gabriel Guerrero <gabriguerreroo@gmail.com>
2021-05-04 12:22:30 -0300

    Projeto 6

---
+++ lab6/Debug/src/gui/lab6_resources.d
@@ -0,0 +1,163 @@
+src/gui/lab6_resources.o: ../src/gui/lab6_resources.c \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/gx_api.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_api.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_port.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el\cm4_gcc\tx_port.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\framework\el/tx_user.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_cmsis.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_version.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_init.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_features.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/gx_port.h \
+ C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\framework\el/gx_user.h \
+ ../src/gui/lab6_resources.h
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/gx_api.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_api.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_port.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el\cm4_gcc\tx_port.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\framework\el/tx_user.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/tx_cmsis.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp/bsp_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_version.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_init.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_leds.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_qspi.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_features.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+c:\users\gabri\documentos\e2_studio\workplace\lab6\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy\ssp\inc\framework\el/gx_port.h:
+
+C:\Users\gabri\Documentos\e2_studio\workplace\lab6\synergy_cfg\ssp_cfg\framework\el/gx_user.h:
+
+../src/gui/lab6_resources.h:
